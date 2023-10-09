1b6b056ad98d0751f5f9c58e838983ca657a177a
Aarzoo Jhamb <aarzoo22008@iiitd.ac.in>
2023-10-09 22:42:31 +0530

    try

--- try.c
+++ try.c
@@ -8,6 +8,48 @@
 
 #include "dummy_main.h"
 
+#define READY 0
+#define RUNNING 1
+
+long long start_ms;
+long long end_ms;
+
+typedef struct Process {
+    pid_t pid;
+    int state;
+    char* name;
+    struct timespec start_time;
+    struct timespec end_time;
+    long long execution_time;
+    float wait_time;
+} process;
+
+typedef struct ready_queue {
+    process p;
+    struct ready_queue* next;
+} queue;
+
+queue* allocate_node(process p) {
+    queue* q = (queue*)malloc(sizeof(queue));
+    q->p = p;
+    q->next = NULL;
+    return q;
+}
+
+queue* insert_rear(queue* head, queue* q) {
+    if (head == NULL) {
+        return q;
+    }
+    queue* tmp = head;
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+    tmp->next = q;
+    return head;
+}
+
+queue* all_process = NULL;
+
 // defining sizes for data structures allocated
 #define INPUT_SIZE 256
 #define HISTORY_SIZE 100
@@ -58,6 +100,16 @@ void displayTerminate()
         printf("%s\nProcess PID: %d\n", record.command, record.process_pid);
         printf("Start time: %s\nEnd Time: %s\nProcess Duration: %f\n", start_time_buffer, end_time_buffer, record.duration);
         printf("--------------------------------\n");
+    }
+
+    queue* head = all_process;
+    while (head != NULL) {
+        process p1 = head->p;
+        printf("Process ID: %d\n", p1.pid);
+        printf("State: %d\n", p1.state);
+        printf("Name: %s\n", p1.name);
+        printf("Execution Time (ms): %lld\n", p1.execution_time);
+        head = head->next;
     }
 }
 
@@ -420,7 +472,15 @@ void shell_loop()
             char **args = tokenize(command, " ");
             strcpy(history.record[history.historyCount].command, tmp);
             history.record[history.historyCount].start_time = time(NULL);
-            status = launch(args);
+            if (strcmp(args[0],"submit")){
+                process p;
+                p.name = args[1];
+                p.state = READY;
+                all_process = insert_rear(all_process, allocate_node(p));
+            }
+            else{
+                status = launch(args);
+            }         
             history.record[history.historyCount].end_time = time(NULL);
             history.record[history.historyCount].duration = difftime(
                 history.record[history.historyCount].end_time,
@@ -433,54 +493,55 @@ void shell_loop()
 }
 
 
-//scheduler
-void scheduler()
-{
-    while (true)
-    {
-        lock(process_table);
-        foreach (Process p
-                 : scheduling_algorithm(process_table))
-        {
-            if (p->state != READY)
-            {
-                continue;
-            }
-            p->state = RUNNING;
-            unlock(process_table);
-            swtch(scheduler_process, p);
-            // p is done for now..
-            lock(process_table);
-        }
-        unlock(process_table);
-    }
-}
+// //scheduler
+// void scheduler()
+// {
+//     while (true)
+//     {
+//         lock(process_table);
+//         foreach (Process p
+//                  : scheduling_algorithm(process_table))
+//         {
+//             if (p->state != READY)
+//             {
+//                 continue;
+//             }
+//             p->state = RUNNING;
+//             unlock(process_table);
+//             swtch(scheduler_process, p);
+//             // p is done for now..
+//             lock(process_table);
+//         }
+//         unlock(process_table);
+//     }
+// }
 
 
-//scheduling algorithm
-void scheduling_algorithm(){
 
-}
+// //scheduling algorithm
+// void scheduling_algorithm(){
+
+// }
 
 
-//Launch function
-void launch(int c , void scheduler){
-
-}
+// //Launch function
+// void launch(int c , void scheduler){
+        
+// }
 
 
 // Main function
 int main(int argc, char *argv[])
 {
     NCPU = atoi(argv[1]);
-    TSLICE = atoi(argv[2]);
+    TSLICE = atof(argv[2]);
 
-    for (int cpu = 1; cpu < NCPU; cpu++)
-    {
-        launch(cpu, scheduler);
-    }
+    // for (int cpu = 1; cpu < NCPU; cpu++)
+    // {
+    //     launch(cpu, scheduler);
+    // }
 
-    scheduler();
+    // scheduler();
     // initializing count for elements in history
     history.historyCount = 0;
     shell_loop();
