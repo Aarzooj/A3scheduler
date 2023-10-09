19c0ad6909069ab70de8fc457d63ce8d4015187e
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-10-09 22:15:44 +0530

    queue

--- try2.c
+++ try2.c
@@ -1,51 +1,90 @@
 #include <stdio.h>
-#include <setjmp.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
-#define MAX_PROCESSES 2
+#define READY 0
+#define RUNNING 1
 
-typedef struct {
-    jmp_buf context;
+long long start_ms;
+long long end_ms;
+
+typedef struct Process {
+    pid_t pid;
     int state;
-} Process;
+    char* name;
+    struct timespec start_time;
+    struct timespec end_time;
+    long long execution_time;
+    float wait_time;
+} process;
 
-Process processes[MAX_PROCESSES];
-int current_process = 0;
+typedef struct ready_queue {
+    process p;
+    struct ready_queue* next;
+} queue;
 
-void scheduler() {
-    int next_process;
-
-    while (1) {
-        next_process = (current_process + 1) % MAX_PROCESSES;
-
-        if (processes[next_process].state == 0) {
-            // Save the context of the current process
-            if (setjmp(processes[current_process].context) == 0) {
-                processes[current_process].state = 1;
-
-                // Restore the context of the next process
-                longjmp(processes[next_process].context, 1);
-            }
-        }
-
-        // Switch to the next process
-        current_process = next_process;
-    }
+queue* allocate_node(process p) {
+    queue* q = (queue*)malloc(sizeof(queue));
+    q->p = p;
+    q->next = NULL;
+    return q;
 }
 
-void example_task(int id) {
-    while (1) {
-        printf("Process %d is running\n", id);
+queue* insert_rear(queue* head, queue* q) {
+    if (head == NULL) {
+        return q;
     }
+    queue* tmp = head;
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+    tmp->next = q;
+    return head;
 }
 
 int main() {
-    // Initialize the processes
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        processes[i].state = 0;
-    }
+    // Get the start time
+    queue* all_process = NULL;
+    process p1;
+    p1.pid = getpid(); // Get the current process ID
+    p1.state = READY;
+    p1.name = "./a.out";
+    clock_gettime(CLOCK_REALTIME, &p1.start_time);
+    sleep(2);
+    // Get the end time
+    clock_gettime(CLOCK_REALTIME, &p1.end_time);
 
-    // Start the scheduler
-    scheduler();
+    // Calculate execution time in milliseconds
+    start_ms = p1.start_time.tv_sec * 1000LL + p1.start_time.tv_nsec / 1000000LL;
+    end_ms = p1.end_time.tv_sec * 1000LL + p1.end_time.tv_nsec / 1000000LL;
+    p1.execution_time = end_ms - start_ms;
+
+    all_process = insert_rear(all_process, allocate_node(p1));
+
+    process p2;
+    p2.pid = getpid(); // Get the current process ID
+    p2.state = READY;
+    p2.name = "./fib";
+    clock_gettime(CLOCK_REALTIME, &p2.start_time);
+    sleep(1);
+    // Get the end time
+    clock_gettime(CLOCK_REALTIME, &p2.end_time);
+    start_ms = p2.start_time.tv_sec * 1000LL + p2.start_time.tv_nsec / 1000000LL;
+    end_ms = p2.end_time.tv_sec * 1000LL + p2.end_time.tv_nsec / 1000000LL;
+    p2.execution_time = end_ms - start_ms;
+
+    all_process = insert_rear(all_process, allocate_node(p2));
+
+    queue* head = all_process;
+    while (head != NULL) {
+        process p1 = head->p;
+        printf("Process ID: %d\n", p1.pid);
+        printf("State: %d\n", p1.state);
+        printf("Name: %s\n", p1.name);
+        printf("Execution Time (ms): %lld\n", p1.execution_time);
+        head = head->next;
+    }
 
     return 0;
 }
